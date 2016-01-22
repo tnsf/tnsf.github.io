d8145830c8ab4541f56be9c4260020fb93c038bb
Graeme Hiebert <graeme.hiebert@safe.com>
2016-01-22 08:33:49 -0800

    Have $scope reflect controller "recipes" object, instead of storing model objects in $scope.

--- recipes/recipe_browser.js
+++ recipes/recipe_browser.js
@@ -1,26 +1,25 @@
 var app = angular.module('RecipeBrowser', []);
 
 app.controller('RecipeController', function($scope,$http) {
-   $scope.categories = null;
-   $scope.selectedRecipe = null;
+   var recipes = {}
 
-   app.loadRecipes($scope,$http,'recipes.json');
+   $scope.recipes = recipes;
+   $scope.selectRecipe = function(recipe) { $scope.recipes.selectedRecipe = recipe; };
 
-   $scope.selectRecipe = function(recipe) { $scope.selectedRecipe = recipe; };
+   app.loadRecipes(recipes,$http,'recipes.json');
 });
 
 
-app.loadRecipes = function($scope,$http,filename) {
+app.loadRecipes = function(recipes,$http,filename) {
    // Load the recipe data
 
    $http.get(filename)
    .success(function(data) {
-      var recipes = data;
-
       // Calculate list of categories
 
       var cats = {};
-      recipes.forEach(function (e,i) {
+      selectedRecipe = null;
+      data.forEach(function (e,i) {
          var cat = e.Category;
          if (!(cat in cats)) {
             cats[cat] = [];
@@ -29,13 +28,14 @@ app.loadRecipes = function($scope,$http,filename) {
       });
       for (var cat in cats) {
          cats[cat].sort(function(a,b) { return (a.Recipe < b.Recipe) ? -1 : (a.Recipe == b.Recipe) ? 0 : 1; });
-         if ($scope.selectedRecipe == null) {
-            $scope.selectedRecipe = cats[cat][0];
+         if (selectedRecipe == null) {
+             selectedRecipe = cats[cat][0];
          }
       }
-      $scope.categories = cats;
+      recipes.selectedRecipe = selectedRecipe;
+      recipes.categories = cats;
    })
    .error(function(data,status,error,config){
-      $scope.recipes = [{heading:"Error",description:"Could not load json data"}];
+      recipes = [{heading:"Error",description:"Could not load json data"}];
    });
 };
